ba923d6f97f74c0a7dd1bc5b2b7d92146bbc653c
Penglg <1696195494@qq.com>
2022-10-30 18:01:05 +0800

    feat(dao): 添加RoleDao

---
+++ src/main/java/com/laigaopeng/www/dao/RoleDao.java
@@ -0,0 +1,26 @@
+package com.laigaopeng.www.dao;
+
+import com.laigaopeng.www.pojo.Role;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface RoleDao {
+    @Insert("insert into role values(null, #{name}, #{permissionLevel}, #{desc})")
+    int save(Role role);
+
+    @Delete("delete from role where id=#{id}")
+    int delete(Integer id);
+
+    @Update("update role set name=#{name}, permission_level=#{permissionLevel}, desc=#{desc}")
+    int update(Role role);
+
+    @Select("select * from role where id=#{id}")
+    Role findById(Integer id);
+
+    @Select("select * from role")
+    List<Role> findAll();
+}
