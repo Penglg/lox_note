adc671ff66439b77b6cf7392560eea428f0198a7
Penglg <1696195494@qq.com>
2022-10-30 17:46:45 +0800

    feat(dao): 添加NoteDao

---
+++ src/main/java/com/laigaopeng/www/dao/NoteDao.java
@@ -0,0 +1,28 @@
+package com.laigaopeng.www.dao;
+
+import com.laigaopeng.www.pojo.Note;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface NoteDao {
+
+    @Insert("insert into note values(null, #{tile}, #{userId}, #{content}, null, #{sectionId}, null, null, " +
+            "#{dateTime})")
+    int save(Note note);
+
+    @Delete("delete from note where id = #{id}")
+    int delete(Integer id);
+
+    @Update("update note set title=#{title}, content=#{content}, section_id=#{sectionId}")
+    int update(Note note);
+
+    @Select("select * from note where id = #{id}")
+    Note findById(Integer id);
+
+    @Select("select * from note")
+    List<Note> findAll();
+}
