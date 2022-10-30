95567e84fc9b362f90bd3efb3263c4573861ebdf
Penglg <1696195494@qq.com>
2022-10-30 17:36:04 +0800

    feat(dao): 添加UserDao

---
+++ src/main/java/com/laigaopeng/www/dao/UserDao.java
@@ -0,0 +1,26 @@
+package com.laigaopeng.www.dao;
+
+import com.laigaopeng.www.pojo.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface UserDao {
+    @Insert("insert into user values (null, #{name}, #{account}, #{password}, #{sex}, null, null, #{createDate}})")
+    int save(User user);
+
+    @Delete("delete from user where id = #{id}")
+    int delete(Integer id);
+
+    @Update("update user set name=#{name}, password=#{password}, sex=#{sex} where id = #{id}")
+    int update(User user);
+
+    @Select("select * from user where id = #{id}")
+    User findById(Integer id);
+
+    @Select("select * from user")
+    List<User> findAll();
+}
