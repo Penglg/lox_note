514de314ac845eabdcfd0b7b7e7b6bf32faa967c
Penglg <1696195494@qq.com>
2022-10-30 16:16:50 +0800

    feat(com.laigaopeng.www.config): 添加事务支持

--- src/main/java/com/laigaopeng/www/config/SpringConfig.java
+++ src/main/java/com/laigaopeng/www/config/SpringConfig.java
@@ -3,9 +3,14 @@ package com.laigaopeng.www.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * Spring配置核心类
+ */
 @Configuration
 @PropertySource("classpath:jdbc.properties")
 @Import({JdbcConfig.class})
+@EnableTransactionManagement
 public class SpringConfig {
 }
