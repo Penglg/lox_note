850627e41af0fc03934d75e28caa124a5f01dddf
Penglg <1696195494@qq.com>
2022-11-01 19:20:51 +0800

    feat(test): 导入spring-test及junit测试坐标，编写userDao测试类

---
+++ src/test/java/com/laigaopeng/www/dao/UserDaoTest.java
@@ -0,0 +1,65 @@
+package com.laigaopeng.www.dao;
+
+import com.laigaopeng.www.config.SpringConfig;
+import com.laigaopeng.www.pojo.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+public class UserDaoTest {
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testSave() {
+        User user = new User();
+        user.setName("saveTest");
+        user.setAccount("saveTest");
+        user.setPassword("123456");
+        user.setSex("男");
+        int result = userDao.save(user);
+        System.out.println("result:" + result);
+    }
+
+    @Test
+    public void testDelete() {
+        int result = userDao.delete(2);
+        System.out.println("result:" + result);
+    }
+
+    @Test
+    public void testUpdate() {
+        User user = new User();
+        user.setName("testUpdate");
+        user.setId(1);
+        user.setPassword("testUpdate");
+        int result = userDao.update(user);
+        System.out.println("result:" + result);
+    }
+
+    @Test
+    public void testFindById() {
+        User user = userDao.findById(1);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testFindAll() {
+        List<User> userList = userDao.findAll();
+        System.out.println(userList);
+    }
+
+    @Test
+    public void testFindUserAndRoleById() {
+        User user = userDao.findUserAndRolesById(1);
+        System.out.println("user:" + user);
+        System.out.println("roles:" + user.getRoles());
+    }
+}
