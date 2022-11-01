66aec58fddeb32a1483bc8ab19ea31bca6c45286
Penglg <1696195494@qq.com>
2022-11-01 19:11:47 +0800

    fix(dao): 修改注解中的sql语句，解决实体类collection和java.util.Collection冲突的问题

--- src/main/java/com/laigaopeng/www/dao/CollectDao.java
+++ src/main/java/com/laigaopeng/www/dao/CollectDao.java
@@ -9,21 +9,21 @@ import java.util.List;
  * 面向collection表的数据访问层
  */
 public interface CollectDao {
-    @Insert("insert into collection values(null, #{userId}, #{noteId})")
+    @Insert("insert into collect values(null, #{userId}, #{noteId})")
     int insert(Collect collection);
 
-    @Delete("delete from collection where id=#{id}")
+    @Delete("delete from collect where id=#{id}")
     int delete(Integer id);
 
-    @Select("select * from collection where id=#{id}")
-    @Results(id = "collectionMap", value = {
+    @Select("select * from collect where id=#{id}")
+    @Results(id = "collectMap", value = {
             @Result(column = "id", property = "id", id = true),
             @Result(column = "user_id", property = "noteId"),
             @Result(column = "note_id", property = "noteId")
     })
     Collect findById(Integer id);
 
-    @Select("select * from collection where user_id=#{userId}")
-    @ResultMap("collectionMap")
+    @Select("select * from collect where user_id=#{userId}")
+    @ResultMap("collectMap")
     List<Collect> findByUserId(Integer userId);
 }
