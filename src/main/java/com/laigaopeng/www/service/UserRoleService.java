a48389f13ac2371724f5e70b8a8173c96bb840ec
Penglg <1696195494@qq.com>
2022-11-01 21:40:29 +0800

    feat(service): 设计基本业务逻辑接口

---
+++ src/main/java/com/laigaopeng/www/service/UserRoleService.java
@@ -0,0 +1,21 @@
+package com.laigaopeng.www.service;
+
+public interface UserRoleService {
+
+    /**
+     * 设置用户角色
+     *
+     * @param userId 用户主键
+     * @param roleId 角色主键
+     * @return 执行结果
+     */
+    boolean setUserRole(Integer userId, Integer roleId);
+
+    /**
+     * 删除用户对应角色
+     *
+     * @param id 用户主键
+     * @return 执行结果
+     */
+    boolean deleteUserRole(Integer id);
+}
