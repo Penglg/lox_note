b5ee4e72a309a0f23b2886671b36d65c030520ec
Penglg <1696195494@qq.com>
2022-11-02 10:27:45 +0800

    style(service): 规范命名

--- src/main/java/com/laigaopeng/www/service/UserRoleService.java
+++ src/main/java/com/laigaopeng/www/service/UserRoleService.java
@@ -9,7 +9,7 @@ public interface UserRoleService {
      * @param roleId 角色主键
      * @return 执行结果
      */
-    boolean setUserRole(Integer userId, Integer roleId);
+    boolean saveUserRole(Integer userId, Integer roleId);
 
     /**
      * 删除用户对应角色
