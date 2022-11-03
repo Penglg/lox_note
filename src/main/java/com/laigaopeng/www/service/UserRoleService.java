d03e089f64f6ac9c78ddb87e6c3d4be5b6f34711
Penglg <1696195494@qq.com>
2022-11-03 21:03:09 +0800

    feat(service): 新增按条件删除功能

--- src/main/java/com/laigaopeng/www/service/UserRoleService.java
+++ src/main/java/com/laigaopeng/www/service/UserRoleService.java
@@ -12,10 +12,18 @@ public interface UserRoleService {
     boolean saveUserRole(Integer userId, Integer roleId);
 
     /**
-     * 删除用户对应角色
+     * 删除用户和角色的绑定
      *
-     * @param id 用户主键
+     * @param id 用户-角色绑定主键
      * @return 执行结果
      */
     boolean deleteUserRole(Integer id);
+
+    /**
+     * 删除角色及角色与用户的绑定
+     *
+     * @param roleId 角色主键
+     * @return 执行结果
+     */
+    boolean deleteRoleUser(Integer roleId);
 }
