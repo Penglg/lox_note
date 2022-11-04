d68fd582995a1e6919dc670b40736235f8f8c498
Penglg <1696195494@qq.com>
2022-11-04 23:02:17 +0800

    test(Service): 添加新增用户业务方法测试

--- src/test/java/com/laigaopeng/www/service/UserServiceTest.java
+++ src/test/java/com/laigaopeng/www/service/UserServiceTest.java
@@ -1,12 +1,27 @@
 package com.laigaopeng.www.service;
 
 import com.laigaopeng.www.config.SpringConfig;
+import com.laigaopeng.www.pojo.User;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringConfig.class)
 public class UserServiceTest {
+    @Autowired
+    private UserService userService;
 
+    @Test
+    public void save() {
+        User user = new User();
+        user.setName("saveTest");
+        user.setAccount("saveTest");
+        user.setPassword("saveTest");
+        user.setSex("男");
+        Boolean result = userService.save(user);
+        System.out.println("com.laigaopeng.www.service.UserServiceTest.save: " + result);
+    }
 }
