b17b36e5803509a1129e1962698b4f746274de26
Penglg <1696195494@qq.com>
2022-11-05 11:49:36 +0800

    pref(service): 优化判断标签名重复的处理

--- src/main/java/com/laigaopeng/www/service/impl/TagServiceImpl.java
+++ src/main/java/com/laigaopeng/www/service/impl/TagServiceImpl.java
@@ -3,6 +3,7 @@ package com.laigaopeng.www.service.impl;
 import com.laigaopeng.www.dao.TagDao;
 import com.laigaopeng.www.pojo.Tag;
 import com.laigaopeng.www.service.TagService;
+import com.laigaopeng.www.util.EmptyCheckerUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,7 +32,10 @@ public class TagServiceImpl implements TagService {
 
     @Override
     public boolean update(Tag tag) {
-        if (isNameRepeat(tag.getName())) return false;
+        // 判断名不为空时是否重复
+        if ((!EmptyCheckerUtil.isStringEmpty(tag.getName())) && isNameRepeat(tag.getName())) {
+            tag.setName(tagDao.getById(tag.getId()).getName()); // 修改为原名
+        }
         return tagDao.update(tag) == 1;
     }
 
