bd19071e04b783bf3c5333f30461246bdf7e2741
Penglg <1696195494@qq.com>
2022-11-05 20:48:19 +0800

    test(service): 添加LikeService测试用例

---
+++ src/test/java/com/laigaopeng/www/service/LikeServiceTest.java
@@ -0,0 +1,88 @@
+package com.laigaopeng.www.service;
+
+import com.laigaopeng.www.config.SpringConfig;
+import com.laigaopeng.www.dao.LikeDao;
+import com.laigaopeng.www.dao.NoteDao;
+import com.laigaopeng.www.pojo.Like;
+import com.laigaopeng.www.pojo.Note;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+public class LikeServiceTest {
+
+    @Autowired
+    private NoteDao noteDao;
+    @Autowired
+    private LikeDao likeDao;
+
+    @Autowired
+    private LikeService likeService;
+
+    @Test
+    public void testSave() {
+        Note note = new Note();
+        note.setTitle("likeSaveTest");
+        note.setContent("likeSaveTest");
+        note.setUserId(0);
+        note.setSectionId(0);
+        noteDao.save(note);
+
+        boolean result = likeService.save(0, note.getId());
+        System.out.println("com.laigaopeng.www.service.LikeServiceTest.testSave: " + result);
+    }
+
+    @Test
+    public void testDelete() {
+        Note note = new Note();
+        note.setTitle("likeDeleteTest");
+        note.setContent("likeDeleteTest");
+        note.setUserId(0);
+        note.setSectionId(0);
+        noteDao.save(note);
+
+        Like like = new Like();
+        like.setNoteId(note.getId());
+        like.setUserId(0);
+        likeDao.save(like);
+
+        boolean result = likeService.delete(like.getId());
+        System.out.println("com.laigaopeng.www.service.LikeServiceTest.testDelete: " + result);
+    }
+
+    @Test
+    public void testDeleteNoteLikes() {
+        Note note = new Note();
+        note.setTitle("likeDeleteTest");
+        note.setContent("likeDeleteTest");
+        note.setUserId(0);
+        note.setSectionId(0);
+        noteDao.save(note);
+
+        likeService.save(-3, note.getId());
+        likeService.save(-2, note.getId());
+
+        boolean result = likeService.deleteNoteLikes(note.getId());
+        System.out.println("com.laigaopeng.www.service.LikeServiceTest.testDeleteNoteLikes: " + result);
+    }
+
+    @Test
+    public void testIsLikeRepeat() {
+        Note note = new Note();
+        note.setTitle("likeRepeatTest");
+        note.setContent("likeRepeatTest");
+        note.setUserId(0);
+        note.setSectionId(0);
+        noteDao.save(note);
+        likeService.save(0, note.getId());
+
+        boolean result1 = likeService.isLikeRepeat(-10, -10);
+        boolean result2 = likeService.isLikeRepeat(0, note.getId());
+        System.out.println("com.laigaopeng.www.service.LikeServiceTest.testIsLikeRepeat: 获取不存在的结果: " +
+                result1 + ", 获取存在的结果: " + result2);
+    }
+}
