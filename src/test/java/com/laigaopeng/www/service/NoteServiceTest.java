a1739a89749a616e0fa2cf1ca8d968febcc5ed21
Penglg <1696195494@qq.com>
2022-11-05 19:41:54 +0800

    test(service): 添加noteService测试用例

---
+++ src/test/java/com/laigaopeng/www/service/NoteServiceTest.java
@@ -0,0 +1,104 @@
+package com.laigaopeng.www.service;
+
+import com.laigaopeng.www.config.SpringConfig;
+import com.laigaopeng.www.dao.NoteDao;
+import com.laigaopeng.www.pojo.Note;
+import com.laigaopeng.www.util.enumhelper.NoteEnum;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+public class NoteServiceTest {
+
+    @Autowired
+    private NoteDao noteDao;
+
+    @Autowired
+    private NoteService noteService;
+
+    @Test
+    public void testSave() {
+        Note note = new Note();
+        note.setTitle("noteSaveTest");
+        note.setContent("noteSaveTest");
+        note.setUserId(0);
+        note.setSectionId(0);
+
+        boolean result = noteService.save(note, null, "noteSaveTest");
+        System.out.println("com.laigaopeng.www.service.NoteServiceTest.testSave: " + result);
+    }
+
+    @Test
+    public void testUpdateNote() {
+        Note note = new Note();
+        note.setTitle("noteUpdateTest");
+        note.setContent("noteUpdateTest");
+        note.setUserId(0);
+        note.setSectionId(0);
+        noteService.save(note, null,  "noteUpdateTest");
+
+        note.setContent("AllForTest!!!");
+        boolean result = noteService.updateNote(note);
+        System.out.println("com.laigaopeng.www.service.NoteServiceTest.testUpdateNote: " + result);
+    }
+
+    @Test
+    public void testDeleteNote() {
+        Note note = new Note();
+        note.setTitle("noteDeleteTest");
+        note.setContent("noteDeleteTest");
+        note.setUserId(0);
+        note.setSectionId(0);
+        noteService.save(note, null,  "noteDeleteTest");
+
+        boolean result = noteService.deleteNote(note.getId());
+        System.out.println("com.laigaopeng.www.service.NoteServiceTest.testDeleteNote: " + result);
+    }
+
+    @Test
+    public void testGetById() {
+        Note note = new Note();
+        note.setTitle("noteGetByIdTest");
+        note.setContent("noteGetByIdTest");
+        note.setUserId(0);
+        note.setSectionId(0);
+        noteService.save(note, null,  "noteGetByIdTest");
+
+        Note result = noteService.getById(note.getId());
+        System.out.println("com.laigaopeng.www.service.NoteServiceTest.testGetById: " + result);
+    }
+
+    @Test
+    public void testListAll() {
+        Note note = new Note();
+        note.setTitle("noteListAllTest");
+        note.setContent("noteListAllTest");
+        note.setUserId(0);
+        note.setSectionId(0);
+        noteService.save(note, null,  "noteListAllTest");
+
+        List<Note> notes = noteService.listAll();
+        System.out.println("com.laigaopeng.www.service.NoteServiceTest.testListAll: " + notes);
+    }
+
+    @Test
+    public void testListLegal() {
+        Note note = new Note();
+        note.setTitle("noteListLegalTest");
+        note.setContent("noteListLegalTest");
+        note.setUserId(0);
+        note.setSectionId(0);
+        noteService.save(note, null,  "noteListLegalTest");
+        note.setLegal(NoteEnum.LEGAL.getCode());
+        noteDao.update(note);
+
+        List<Note> notes = noteService.listAll(NoteEnum.LEGAL.getCode());
+        System.out.println("com.laigaopeng.www.service.NoteServiceTest.testListAllByLegal: " + notes);
+    }
+}
