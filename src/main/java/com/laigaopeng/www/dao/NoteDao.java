09aa9f92e5e6d2d01f6e4d09fc6f0c49bc053ab7
Penglg <1696195494@qq.com>
2022-11-07 20:16:00 +0800

    fix(dao): 表名与sql内置函数冲突

--- src/main/java/com/laigaopeng/www/dao/NoteDao.java
+++ src/main/java/com/laigaopeng/www/dao/NoteDao.java
@@ -69,7 +69,7 @@ public interface NoteDao {
      * @param userId user主键id
      * @return 集合结果
      */
-    @Select("select * from note n, like l where n.id = l.note_id and l.user_id = #{userId}")
+    @Select("select * from note n, `like` l where n.id = l.note_id and l.user_id = #{userId}")
     @ResultMap("noteMap")
     List<Note> listLikes(Integer userId);
 
