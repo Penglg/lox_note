d88188187d141b9e54bae8a359af87707d900bbe
Penglg <1696195494@qq.com>
2022-11-10 23:30:59 +0800

    feat(pojo): 添加返回前端的数据结果类

---
+++ src/main/java/com/laigaopeng/www/pojo/vo/Result.java
@@ -0,0 +1,46 @@
+package com.laigaopeng.www.pojo.vo;
+
+public class Result {
+
+    private Object data; // 返回前端的数据
+    private int code; // 状态码
+    private String msg; // 信息
+
+    public Result() {
+    }
+
+    public Result(Object data, int code) {
+        this.data = data;
+        this.code = code;
+    }
+
+    public Result(Object data, int code, String msg) {
+        this.data = data;
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
