0f4019eeac8855656e83fa8ea517ab4c1ab98276
Penglg <1696195494@qq.com>
2022-11-12 11:27:58 +0800

    feat(controller): 审批申请功能控制层

---
+++ src/main/java/com/laigaopeng/www/controller/ApprovalController.java
@@ -0,0 +1,59 @@
+package com.laigaopeng.www.controller;
+
+import com.laigaopeng.www.pojo.Approval;
+import com.laigaopeng.www.pojo.vo.Result;
+import com.laigaopeng.www.service.ApprovalService;
+import com.laigaopeng.www.util.enumhelper.CodeEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 审批申请业务控制层
+ * 
+ */
+@RestController("approvalController")
+@RequestMapping("/approvals")
+public class ApprovalController {
+    
+    @Autowired
+    private ApprovalService approvalService;
+
+    /**
+     * 增添申请
+     *
+     * @param approval 申请
+     * @return 结果
+     */
+    @PostMapping
+    public Result save(@RequestBody Approval approval) {
+        boolean result = approvalService.save(approval);
+        return result ? new Result(true, CodeEnum.SUCCESS.getCode(), CodeEnum.SUCCESS.getMsg()) :
+                new Result(false, CodeEnum.FAIL.getCode(), CodeEnum.FAIL.getMsg());
+    }
+
+    /**
+     * 删除申请
+     *
+     * @param id 申请
+     * @return 结果
+     */
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable int id) {
+        boolean result = approvalService.delete(id);
+        return result ? new Result(true, CodeEnum.SUCCESS.getCode(), CodeEnum.SUCCESS.getMsg()) :
+                new Result(false, CodeEnum.FAIL.getCode(), CodeEnum.FAIL.getMsg());
+    }
+
+    /**
+     * 处理申请
+     *
+     * @param approval 申请
+     * @return 结果
+     */
+    @PutMapping()
+    public Result handle(@RequestBody Approval approval) {
+        boolean result = approvalService.handle(approval);
+        return result ? new Result(true, CodeEnum.SUCCESS.getCode(), CodeEnum.SUCCESS.getMsg()) :
+                new Result(false, CodeEnum.FAIL.getCode(), CodeEnum.FAIL.getMsg());
+    }
+}
