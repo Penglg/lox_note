29820a7e4aff3e93cb2bd0525810a29490eb9fc7
Penglg <1696195494@qq.com>
2022-11-13 21:07:41 +0800

    feat(config): 添加SpringMVC扩展并配置静态资源的放行

---
+++ src/main/java/com/laigaopeng/www/config/SpringMvcSupport.java
@@ -0,0 +1,24 @@
+package com.laigaopeng.www.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * SpringMVC扩展类
+ * 用于配置资源管理器
+ *
+ */
+@Configuration
+public class SpringMvcSupport extends WebMvcConfigurationSupport {
+    /**
+     * 配置静态资源的放行
+     *
+     * @param registry 静态资源管理器
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
+        registry.addResourceHandler("/WEB-INF/**").addResourceLocations("WEB-INF/");
+    }
+}
