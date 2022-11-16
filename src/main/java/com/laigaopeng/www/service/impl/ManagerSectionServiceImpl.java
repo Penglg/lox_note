9013eac259f7247514483c1c4f2abbd3a2ea1b0c
Penglg <1696195494@qq.com>
2022-11-16 21:03:42 +0800

    feat(*): 添加封管理员功能

--- src/main/java/com/laigaopeng/www/service/impl/ManagerSectionServiceImpl.java
+++ src/main/java/com/laigaopeng/www/service/impl/ManagerSectionServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ManagerSectionImpl implements ManagerSectionService {
+public class ManagerSectionServiceImpl implements ManagerSectionService {
 
     @Autowired
     private ManagerSectionDao managerSectionDao;
